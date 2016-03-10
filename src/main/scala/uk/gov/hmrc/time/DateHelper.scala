79cf2b802d6d03a52b10e4ee9a7e82e923c5a13e
Allen Fensome <allen.fensome@gmail.com>
2016-03-10 09:52:05 +0000

    Initial commit

---
+++ src/main/scala/uk/gov/hmrc/time/DateHelper.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cato.time
+
+import org.joda.time.LocalDate
+
+object DateHelper extends DateHelper
+
+trait DateHelper {
+
+  val fakeTimeOffsetInMillis: Long = {
+    getFakeDateString() match {
+      case Some(s: String) => {
+        val fakeTime = new LocalDate(s).toDateTimeAtStartOfDay().getMillis
+        val currentTime = getCurrentDate().toDateTimeAtStartOfDay.getMillis
+        fakeTime - currentTime
+      }
+      case _ => 0
+    }
+
+  }
+
+  def now(): LocalDate = {
+    getFakeDateString() match {
+      case None => getCurrentDate()
+      case Some(d) => new LocalDate(getCurrentDate().toDateTimeAtCurrentTime.getMillis + getFakeDateOffset())
+    }
+  }
+
+  def getFakeDateString() = {
+    sys.props.get("feature.fakeDate")
+  }
+
+  def isFakeDateActive() = getFakeDateString().nonEmpty
+
+  def getFakeDateOffset(): Long = fakeTimeOffsetInMillis
+
+  // need this so can override in testing
+  protected[time] def getCurrentDate() = new LocalDate()
+}
+
---
+++ src/test/scala/uk/gov/hmrc/time/DateHelperSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cato.time
+
+import org.joda.time.LocalDate
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfter, Matchers, WordSpec}
+
+class DateHelperSpec extends WordSpec with MockitoSugar with Matchers with BeforeAndAfter {
+
+  case class TestDateHelper(fakeCurrentDate: String = new LocalDate().toString) extends DateHelper {
+
+    override def getCurrentDate() = {
+      new LocalDate(fakeCurrentDate)
+    }
+  }
+
+  override protected def before(fun: => Any): Unit = clearFakeDateString()
+
+  "DateHelper" should {
+
+    "return empty fakeTimeOffset when not set" in  {
+      val pretendActualDate: String = "2001-01-01"
+      val dateHelper = new TestDateHelper(pretendActualDate)
+      dateHelper.getFakeDateOffset() shouldBe(0)
+    }
+
+    "return fakeTimeOffset with date in future" in  {
+      val fakeDate: String = "2001-01-02"
+      val pretendActualDate: String = "2001-01-01"
+
+      setFakeDateString(fakeDate)
+      val dateHelper = new TestDateHelper(pretendActualDate)
+
+      dateHelper.getFakeDateOffset() shouldBe(86400000L) //millis in 1 day
+    }
+
+    "return fakeTimeOffset with date in past" in  {
+      val fakeDate: String = "2001-01-01"
+      val pretendActualDate: String = "2001-01-02"
+
+      setFakeDateString(fakeDate)
+      val dateHelper = new TestDateHelper(pretendActualDate)
+
+      dateHelper.getFakeDateOffset() shouldBe(-86400000L) //millis in 1 day
+    }
+
+    "return date specified by sys prop when asked" in {
+      val dateString: String = "2001-01-02"
+      val pretendActualDate: String = "2001-01-01"
+
+      setFakeDateString(dateString)
+      val dateHelper = TestDateHelper(pretendActualDate)
+
+      dateHelper.now() shouldBe new LocalDate(dateString)
+    }
+
+    "return date specified by sys prop when asked (always tomorrow)" in {
+
+      val dateString: String = new LocalDate().plusDays(1).toDateTimeAtStartOfDay.toLocalDate.toString
+      setFakeDateString(dateString)
+
+      val dateHelper = TestDateHelper()
+      dateHelper.now() shouldBe new LocalDate().plusDays(1).toDateTimeAtStartOfDay().toLocalDate
+    }
+
+  }
+
+
+  private def clearFakeDateString() = sys.props.remove("feature.fakeDate")
+
+  private def setFakeDateString(dateString: String): Option[String] = sys.props.put("feature.fakeDate", dateString)
+}
